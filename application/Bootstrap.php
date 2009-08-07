18e78f585d06a0deff49b61f6bf4cb7791fdac47
akeem philbert <akeemphilbert@gmail.com>
2009-08-07 06:41:10 -0400

    Imported albert's code

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,117 @@
 <?php
-
+/**
+ * 
+ * @author Albert Rosa
+ *
+ */
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+	/**
+	 * This will autoload based on the folder structure to files
+	 * 
+	 * @return Zend_Application_Module_Autoloader
+	 */
+	protected function _initAutoload(){
+		$autoloader = new Zend_Application_Module_Autoloader(array(
+            'namespace' => 'Default_',
+            'basePath'  => dirname(__FILE__),
+        ));
+        return $autoloader;
+		
+	}	
+	
+	/**
+	 * Added page caching to the site
+	 * (non-PHPdoc)
+	 * @see library/Zend/Application/Bootstrap/Zend_Application_Bootstrap_Bootstrap#run()
+	 */
+	public function run(){
+		
+		$this->setupCache();
+		$this->setUpLocale();	
+		$this->setupRoutes();	
+		$this->setupLoggers();
+		parent::run();
+			
+	}
+	
+	/**
+	 * This is where we set up the caching for the applcation
+	 * 
+	 * @todo currently the memcachedoptions are not working 
+	 * 
+	 * @return void
+	 */
+	private function setupCache()
+    {	
+    	$config  = new Zend_Config_Xml(APPLICATION_PATH.'/configs/cache.xml',APPLICATION_ENV );
+    	
+        $cache = Zend_Cache::factory(
+        	$config->cache->frontend->name,        	
+        	$config->cache->backend->name,
+        	$config->cache->frontend->toArray(),        	
+        	$config->cache->backend->toArray()
+        );
+        $cache->start();
+    }
+    
+    /**
+     * This is where we set up the locale
+     * 
+     * @return void
+     */
+    private function setUpLocale()
+    {
+    	//this is the locale		
+		try {
+    		$locale = new Zend_Locale(Zend_Locale::BROWSER);
+		} catch (Zend_Locale_Exception $e) {
+    		$locale = new Zend_Locale('de');
+    		
+		}
+		Zend_Locale::setDefault($locale);
+		
+    }
+    
+    /**
+     * This is where we set up the routes
+     * 
+     * @return void
+     */
+    private function setupRoutes(){
+    	$config  = new Zend_Config_Ini(APPLICATION_PATH.'/configs/routes.ini',APPLICATION_ENV );
+		$router = new Zend_Controller_Router_Rewrite();
+		$router->addConfig($config, 'routes');
+		
+		$controller = Zend_Controller_Front::getInstance();
+		$controller->setRouter($router);
+    }
+    
+    
+    /**
+     * Here we set the loggers for logging errors
+     * @todo add zend_log_writer_email 
+     * @return void
+     */
+    private function setupLoggers(){
+    	$config  = new Zend_Config_Ini(APPLICATION_PATH.'/configs/application.ini',APPLICATION_ENV );
+    	$today = Zend_Date::now(); 
+    	
+    	$writer = new Zend_Log_Writer_Stream($config->log->location.$today->get('M_d_Y').'.txt');
 
-
+    	$fbug = new Zend_Log_Writer_Firebug();
+    	
+    	$logger = new Zend_Log($writer);
+    	$logger->addWriter($fbug);
+    	
+    	Zend_Registry::set('logger',$logger);
+    	
+    	
+    		
+    }
+    
+    
+    
+    
 }
 
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'Ifphp/models/Supports.php';
 
 class IndexController extends Zend_Controller_Action
 {
@@ -11,7 +12,52 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+//        //here I will be testing the models
+//        $user = new Ifphp\models\Users();
+//        $result = $user->getById(2);
+//        echo sizeof($result);
+//        echo $result->username;
+        
     }
+    
+    /**
+     * This renders the about page
+     * @return void
+     */
+    public function aboutAction()
+    {
+    	
+    }
+    
+    /**
+     * This renders the support page
+     * @return void
+     */
+    public function supportAction()
+    {    	
+        //here I will be testing the models
+        $supports = new Supports();
+        if($this->getRequest()->isGet() && 
+        	$this->getRequest()->getParam('term') != null
+        	
+        ){
+        	$term = $this->getRequest()->getParam('term');        		
+        	$result = $supports->search($term);
+        	$this->view->supports = $result;
+        	$this->view->term = $term;
+        }
+        
+    }
+    
+    public function clearAction()
+    {
+    	$supports = new Supports();
+    	$supports->clear();
+    }
+    
+    
+    
+    
 
 
 }
