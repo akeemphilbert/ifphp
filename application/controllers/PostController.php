61c206c859cbdd52d81dc3bf37344ead01ed8c37
akeem philbert <akeemphilbert@gmail.com>
2009-08-09 21:46:16 -0400

    Added feed parsing on feed submit

---
+++ application/controllers/PostController.php
@@ -0,0 +1,13 @@
+<?php 
+class PostController extends Zend_Controller_Action
+{
+	/**
+	 * View a post. This updates the view stats and redirects to the external post
+	 * 
+	 * @return unknown_type
+	 */
+	public function viewAction()
+	{
+		
+	}
+}
