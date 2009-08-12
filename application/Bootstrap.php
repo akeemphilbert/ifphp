aecf3d1f7b12dacbec513bc96e32226ca352b395
Albert Rosa <rosalbert@gmail.com>
2009-08-12 01:34:02 -0400

    Added Localization for English and Spanish for complete site Forms also take advantage of the translate adapter

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -34,16 +34,16 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 	
 	/**
 	 * Run custom setup functions. This was abstracted from run for Unit Testing purposes
-	 * 
+	 * NOTE: order is important loggers must be before locale due to logging locale errors 
 	 * @return void
 	 */
 	protected function _bootstrap($resource=null)
 	{
 		parent::_bootstrap($resource);
-		
-		$this->setUpLocale();	
-		$this->setupRoutes();	
+
 		$this->setupLoggers();
+		$this->setUpLocale();	
+		$this->setupRoutes();
 		$this->setupIdentity();
 		$this->setupViewHelpers();		
 	}
@@ -66,10 +66,11 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         	$config->cache->backend->toArray()
         );
         $cache->start();
+        
     }
     
     /**
-     * This is where we set up the locale
+     * This is where we set up the locale and set the translation adapters
      * 
      * @return void
      */
@@ -79,10 +80,26 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 		try {
     		$locale = new Zend_Locale(Zend_Locale::BROWSER);
 		} catch (Zend_Locale_Exception $e) {
-    		$locale = new Zend_Locale('de');
+    		$locale = new Zend_Locale('en_US');
     		
 		}
 		Zend_Locale::setDefault($locale);
+    	
+		// here we define the translator
+		$translatePath = APPLICATION_PATH.'/languages/translate.xml';
+    	    
+    	$options = array('log'=>Zend_Registry::get('logger'),'disableNotices'=>false, 
+    		'logMessage'=>"Untranslated message within '%locale%': %message% : %word%");
+    	$translate = new Zend_Translate('tmx',
+			$translatePath,$locale, $options
+		);
+		
+		// hwere we add the translator to the registry
+		Zend_Registry::set('translate', $translate);
+		
+		// here we set all forms attached to the translator
+		// this will translate everything on the form
+		Zend_Form::setDefaultTranslator($translate);
 		
     }
     
@@ -119,6 +136,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     	$logger->addWriter($fbug);
     	
     	Zend_Registry::set('logger',$logger);
+    	
     		
     }
     
@@ -146,6 +164,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 		Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
     	
     }
+    
     
     
     
