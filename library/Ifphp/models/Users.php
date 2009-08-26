5cc2600eb5846165fe50f4c53d8b6851bcc161de
Akeem <akeem@akeem-laptop.(none)>
2009-08-25 21:15:34 -0400

    Added resend activation code, facilitated a user having more than one feed

--- library/Ifphp/models/Users.php
+++ library/Ifphp/models/Users.php
@@ -5,7 +5,7 @@
  * to do with the user
  * 
  * @version 0.1
- * @author 
+ * @author Akeem Philbert <akeemphilbert@gmail.com>
  * @package Ifphp
  * @subpackage models
  * @copyright 2009 Ifphp
@@ -18,7 +18,20 @@ require_once 'Ifphp/core/AbstractModel.php';
 /**
  * This class contains all the db interactions for the user
  */
-class Users extends AbstractModel{
+class Users extends AbstractModel
+{
 	protected $_name = 'users';
-	
+
+    /**
+     * Get user by email
+     *
+     * @param string $email
+     * @return User
+     */
+    public function getByEmail($email)
+    {
+        $select = $this->select();
+        $select->where('email = ?', $email);
+        return $this->fetchRow($select);
+    }
 }
